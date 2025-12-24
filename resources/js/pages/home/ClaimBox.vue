e0deba9cd43309af408b799523b2b99a186d153f
koonran69 <koonran69@gmail.com>
2025-12-24 09:13:53 +0700

    done lucky message random

--- resources/js/pages/home/ClaimBox.vue
+++ resources/js/pages/home/ClaimBox.vue
@@ -26,12 +26,30 @@ const showRewardModal = ref(false);
 const currentReward = ref<RewardHistory | null>(null);
 
 const { t } = useI18n();
+const luckyMessages = [
+  'Chúc bạn tiền vào cửa trước, tiền lẻn cửa sau, hai cái gặp nhau chui vào két sắt',
+  'Chúc bạn gấm vóc lụa là, tay cầm tiền tỉ được làm đại gia',
+  'Chúc bạn năm mới đa lộc, đa tài, đa phú quý. Đắc thời, đắc thắng, đắc nhân tâm',
+  'Chúc bạn vạn sự như ý, tỉ sự như mơ, triệu triệu bất ngờ, không chờ cũng đến',
+  'Chúc bạn tiền vào như nước triều dâng, tiêu hoài không cạn, càng xài càng tăng',
+  'Chúc bạn một năm mới hạnh phúc, khum “chằm Zn”, âu lo. Đầu năm phát tài, cuối năm tài vẫn phát',
+  'Chúc bạn bầu trời sức khỏe, biển cả tình thương, đại dương tình bạn, sự nghiệp sáng ngời, gia đình thịnh vượng',
+  'Chúc bạn vui vẻ như Chim Sẻ, khỏe mạnh như Đại Bàng, giàu sang như chim Phụng, làm lụng như chim Sâu, sống lâu như Đà Điểu',
+  'Chúc bạn tiền vào như nước sông Đà, tiền ra nhỏ giọt như cà phê phin',
+  'Chúc bạn và gia đình luôn thuận hòa, song thân đắc thọ, tỷ sự như mơ, vạn sự như ý',
+  'Chúc bạn năm mới công thành danh toại, trẻ mãi không già, tấn tài, tấn lộc, phúc lộc trường tồn',
+  'Năm mới chúc bạn sức khỏe đầy đủ, gạo đầy lu, muối đầy hũ, tiền đầy túi, tình đầy tim, xăng đầy bình, và vàng đầy tủ!'
+];
+const randomLuckyMessage = () => {
+  const index = Math.floor(Math.random() * luckyMessages.length);
+  return t(luckyMessages[index]);
+};
 const rewardDesc = computed(() => {
   if (!currentReward.value) return null;
   const code = props.openedReward.reward.code;
   switch (code) {
     case 'LUCKY_MESSAGE':
-      return t('Hành trình mang Tết về nhà của bạn vẫn tiếp tục ✨Khám phá thêm các mẫu vali Bamozo để gói trọn những chuyến đi sắp tới nhé!');
+      return randomLuckyMessage();
     case 'SECOND_PRIZE_CAMERA':
     case 'THIRD_PRIZE_MIBAND':
     case 'CONSOLATION_PILLOW':
